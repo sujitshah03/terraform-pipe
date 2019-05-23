17ad7f143a226e066af5f7cd26cd0748b1152094
Jay Shah <47807540+shahjayone123@users.noreply.github.com>
2019-05-22 23:32:34 -0400

    Update subnet.tf

--- east/subnet.tf
+++ east/subnet.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "main" {
-  vpc_id     = "${aws_vpc.main.id}"
+  vpc_id     = "default"
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Main"
+    Name = "my-subnet"
   }
 }
 »
